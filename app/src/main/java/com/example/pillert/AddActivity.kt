641d5935440a743293f115aa0c602079bf74599b
NaRyung Kim <criticalpxint@naver.com>
2023-06-18 15:23:56 +0900

    230617 다국어 지원(한국어, 영어, 일본어)

--- app/src/main/java/com/example/pillert/AddActivity.kt
+++ app/src/main/java/com/example/pillert/AddActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        setContentView(R.layout.activity_add2)
     }
 }
